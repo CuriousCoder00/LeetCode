d52745d3e5303378ba9643cc54433e686dcf0fd5
Kapil Jangid <kapiljangid040@gmail.com>
2024-07-23 14:12:54 +0530

    chore: add Roman to Integer solution

---
+++ Easy/#13_Roman_To_Integer.js
@@ -0,0 +1,15 @@
+var romanToInt = function (s) {
+  const symbols = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  let res = 0;
+  for (let i = 0; i < s.length; i++) {
+    const curr = symbols[s[i]];
+    const next = i + 1 < s.length ? symbols[s[i + 1]] : 0;
+    if (curr < next) {
+      res += next - curr;
+      i++;
+    } else {
+      res += curr;
+    }
+  }
+  return res;
+};
